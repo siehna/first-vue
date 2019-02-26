b5775f4de3472eceae4b6182122f3edd2d30a5fa
siehna <bq13026@gmail.com>
2019-02-26 13:11:24 +0900

    vuetify 導入のみ

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuetify from'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Vuetify)
 
-//完全に空
+//完全に空 EventHub
 window.eventHub = new Vue()
 
 /* eslint-disable no-new */
