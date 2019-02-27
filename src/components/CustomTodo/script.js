c956e50bd3de72f9016462d851e84e75adb3f821
siehna <bq13026@gmail.com>
2019-02-27 10:34:32 +0900

    ファイル分割終了 課題ひとまず終わり！

---
+++ src/components/CustomTodo/script.js
@@ -0,0 +1,37 @@
+export default {
+  name: 'hello',
+  data: function () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      todos : [
+        {text : 'vue-router', done: false, editing: false},
+        {text : 'vuex', done: false, editing: false},
+        {text : 'vue-loader', done: false, editing: false},
+        {text : 'awesome-vue', done: true, editing: false},
+      ],
+      newTodo: ""
+    }
+  },
+  methods: {
+    addTodo: function(event) {
+      let text = this.newTodo && this.newTodo.trim()
+      if (!text) {
+        return
+      }
+      this.todos.push({
+        text: text,
+        done: false,
+        editing: false
+      })
+      this.newTodo = ''
+    },
+    removeTodo: function (event) {
+      for (let i = this.todos.length - 1; i >= 0; i--) {
+        if (this.todos[i].done) this.todos.splice(i, 1)
+      }
+    }
+  },
+  mounted:function () {
+    eventHub.$emit('change-title','CustomTodo' )
+  }
+}
