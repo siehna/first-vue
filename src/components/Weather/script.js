4d087db5ebb76e85a9439819c815aefa57bd5848
siehna <bq13026@gmail.com>
2019-02-28 17:17:47 +0900

    天気予報APIはまだ取得できず ページ作成とaxios.get～例外までは書いた

--- src/components/Weather/script.js
+++ src/components/Weather/script.js
@@ -1,6 +1,17 @@
+import axios from 'axios';
 export default{
+  // el:'weather',
   name:'weather-component',
-  mounted:function () {
-    eventHub.$emit('change-title','Weather' )
+  data(){
+    return{
+      info:'no response'
+    }
+  },
+  mounted:function (){
+    eventHub.$emit('change-title','Weather' );
+    axios
+      .get('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+      .then(response => {this.info=response})
+      .catch(err => {this.info='fault to get API'});
   }
 }
