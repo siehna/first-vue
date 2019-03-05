1810ba8f687b77556bc8670902d0c4f0db3a9d19
siehna <bq13026@gmail.com>
2019-03-05 15:42:04 +0900

    getメソッドをmounted method に作成 whenは更新されていた、後は click の度にgetを呼べば良い

--- src/components/Weather/script.js
+++ src/components/Weather/script.js
@@ -4,7 +4,6 @@ export default{
   name:'weather-component',
   data(){
     return{
-      info:'no response',
       dateLabel:'empty',
       telop:'no data',
       date:'no data',
@@ -12,35 +11,27 @@ export default{
       min:null,
       max:null,
       iconUrl:null,
+      bottomNav: '',
       //0:今日 1:明日 2:明後日
-      when: 1,
-      bottomNav: 'recent',
-      items: [
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'
-        },
-        {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'
-        }
-      ]
+      when: 1
     }
   },
   method:{
-    //whenの設定
-    setWhen0:function(){
-      this.when-0
-    },
-    setWhen1:function () {
-      this.when=1
-    },
-    setWhen2:function () {
-      this.when=2
+    getData: function () {
+      axios
+        .get('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010')
+        .then(response => {
+          this.dateLabel=response['data']['forecasts'][this.when]["dateLabel"]
+          this.telop=response['data']['forecasts'][this.when]["telop"]
+          this.iconUrl=response['data']['forecasts'][this.when]["image"]["url"]
+          this.date=response['data']['forecasts'][this.when]["date"]
+          this.min=response['data']['forecasts'][this.when]["temperature"]["min"]["celsius"]
+          this.max=response['data']['forecasts'][this.when]["temperature"]["max"]["celsius"]
+          this.text=response['data']['description']['text']
+          this.info=response['data']['forecasts'][this.when]
+          console.log(response)
+        })
+        .catch(err => {this.info='fault to get API'});
     }
   },
   mounted:function (){
