5b53ac5d827da898085ae5b764ee067e1e657ba6
siehna <bq13026@gmail.com>
2019-03-08 14:32:14 +0900

    連想配列の表示が[object, Object]でマズい

--- src/components/Weather/script.js
+++ src/components/Weather/script.js
@@ -8,8 +8,8 @@ export default{
       telop:'no data',
       date:'no data',
       text:'no text',
-      // max:['empty','empty','empty'],
-      // min:['empty','empty','empty'],
+      max:'',
+      min:'',
       iconUrl:null,
       cityCode:130010,
       bottomNav: '',
@@ -19,11 +19,7 @@ export default{
 
 
       dropdown_wide: [],
-      dropdown_close: [
-        { text: 'list', callback: () => console.log('list') },
-        { text: 'favorite', callback: () => console.log('favorite') },
-        { text: 'delete', callback: () => console.log('delete') }
-      ],
+      dropdown_close: [],
 
 
       num: 1
@@ -35,55 +31,69 @@ export default{
 
   methods:{
     // クリックするとAPI取得ができるメソッド
-    pickUpData: function () {
-      for(var num=0; num<2; num++){
-        axios
-          .get('http://weather.livedoor.com/forecast/webservice/json/v1?city='+this.cityCode)
-          .then(response => {
-            this.dateLabel=response['data']['forecasts'][num]["dateLabel"]
-            this.telop=response['data']['forecasts'][num]["telop"]
-            this.iconUrl=response['data']['forecasts'][num]["image"]["url"]
-            this.date=response['data']['forecasts'][num]["date"]
-            // this.min=response['data']['forecasts'][num]["temperature"]["min"]["celsius"]
-            // this.max=response['data']['forecasts'][num]["temperature"]["max"]["celsius"]
-            this.text=response['data']['description']['text']
-            this.info=response['data']['forecasts'][num]
-            // console.log(response)
-          })
-          .catch(err => {this.info='fault to get API'});
-      }
+    //天気情報の取得
+    getWeatherData: function () {
+      // リストIdから情報の取得
+      //APIの読み込み
+      axios
+        .get('http://weather.livedoor.com/forecast/webservice/json/v1?city=' + this.cityCode)//Idにすると404が出る、要質問
+        .then(response => {
+          for(var num=0; num<response['data']['forecasts'].length; num++) {
+            this.dateLabel = response['data']['forecasts'][this.num]["dateLabel"]
+            this.telop = response['data']['forecasts'][this.num]["telop"]
+            // this.iconUrl = response['data']['forecasts'][this.num]["image"]["url"]
+            // this.date = response['data']['forecasts'][this.num]["date"]
+            this.min=response['data']['forecasts'][num]["temperature"]["min"]["celsius"]
+            this.max=response['data']['forecasts'][num]["temperature"]["max"]["celsius"]
+            this.text = response['data']['description']['text']
+            // this.info=response['data']['forecasts'][this.num]
+            console.log(response['data'])
+          }
+        })
+        .catch(err => {
+          this.info = 'fault to get API'
+        })
     },
 
 
 
+    //XMLからリスト情報の取得
     getLocation: function(){
       axios
         .get('http://weather.livedoor.com/forecast/rss/primary_area.xml',{responseType:'document'})
-        .then(responseXml =>{
+    .then(responseXml =>{
           //広域エリアリストにプッシュ（配列）
           var prefLength = responseXml['data'].getElementsByTagName('pref').length
           for(var i=0; i<prefLength; i++){
             this.$set(this.dropdown_wide, i, responseXml['data'].getElementsByTagName('pref')[i].attributes['title'].textContent)
-            //小規模エリアリストにプッシュ（連想配列）
+            //小規模エリアリストにプッシュ（連想配列）ひとまず東京の情報を取得 16=東京
             var childrenLength=responseXml['data'].getElementsByTagName('pref')[i].getElementsByTagName('city').length
             for(var j=0; j<childrenLength; j++){
-
+              this.dropdown_close.push({
+                area:responseXml['data'].getElementsByTagName('pref')[i].attributes['title'].textContent,
+                title: responseXml['data'].getElementsByTagName('pref')[i].getElementsByTagName('city')[j].attributes['title'].textContent,
+                id: responseXml['data'].getElementsByTagName('pref')[i].getElementsByTagName('city')[j].attributes['id'].textContent,
+                callback: () => console.log(responseXml['data'].getElementsByTagName('pref')[i].getElementsByTagName('city')[j].attributes['title'].textContent)
+              })
             }
           }
+          console.log('wide : '+ this.dropdown_wide)
+          console.log(childrenLength)
+          console.log(this.dropdown_close)
 
 
-
-          this.areaName=responseXml['data'].getElementsByTagName('pref')[20].attributes['title'].textContent
-          this.cityName=responseXml['data'].getElementsByTagName('pref')[20].getElementsByTagName('city')[0].attributes['title'].textContent
-          this.cityId=responseXml['data'].getElementsByTagName('pref')[20].getElementsByTagName('city')[0].attributes['id'].textContent+''
-          // console.log(responseXml)
-          console.log(this.dropdown_wide)
-          console.log(this.areaName)
-          console.log(this.cityName)
-          console.log(this.cityId)
-          console.log(this.cityCode)
+          // this.areaName=responseXml['data'].getElementsByTagName('pref')[20].attributes['title'].textContent
+          // this.cityName=responseXml['data'].getElementsByTagName('pref')[20].getElementsByTagName('city')[0].attributes['title'].textContent
+          // this.cityId=responseXml['data'].getElementsByTagName('pref')[20].getElementsByTagName('city')[0].attributes['id'].textContent+''
+          // // console.log(responseXml)
+          // console.log(this.dropdown_wide)
+          // console.log(this.areaName)
+          // console.log(this.cityName)
+          // console.log('city ID : '+this.cityId)
+          // console.log('city Code : '+this.cityCode)
         })
-    },
+    }
+
 
 
   },
@@ -98,28 +108,5 @@ export default{
 
     //リスト情報の取得
     this.getLocation();
-
-
-    //天気情報の取得
-
-
-
-
-
-    // for(var num=0; num<3; num++){
-    // axios
-    //   .get('http://weather.livedoor.com/forecast/webservice/json/v1?city='+this.cityCode)//Idにすると404が出る、要質問
-    //   .then(response => {
-    //     this.dateLabel=response['data']['forecasts'][this.num]["dateLabel"]
-    //     this.telop=response['data']['forecasts'][this.num]["telop"]
-    //     this.iconUrl=response['data']['forecasts'][this.num]["image"]["url"]
-    //     this.date=response['data']['forecasts'][this.num]["date"]
-    //     // this.min=response['data']['forecasts'][num]["temperature"]["min"]["celsius"]
-    //     // this.max=response['data']['forecasts'][num]["temperature"]["max"]["celsius"]
-    //     this.text=response['data']['description']['text']
-    //     // this.info=response['data']['forecasts'][this.num]
-    //     // console.log(response)
-    //   })
-    //   .catch(err => {this.info='fault to get API'})
   }
 }
