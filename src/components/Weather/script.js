c553e942fc0392e07c0791ecdd7e029acd541a15
siehna <bq13026@gmail.com>
2019-03-11 17:02:24 +0900

    jsonデータの取得ができた。 後は3日分のデータを表示する感じ。

--- src/components/Weather/script.js
+++ src/components/Weather/script.js
@@ -3,22 +3,32 @@ export default{
   name:'weather-component',
   data(){
     return{
+      //表示情報
       info:'',
       dateLabel:'empty',
       telop:'no data',
       date:'no data',
       text:'no text',
-      max:'',
-      min:'',
+      max:'-',
+      min:'-',
       iconUrl:null,
-      cityCode:130010,
+
+      //検証用データ
       bottomNav: '',
+
+      //選択エリア・都市名
       area:"",
+      city:"",
+
+      //リクエスト用URL
+      requestUrl:"",
+
+      //XML情報
       areaName:'',
       cityName:'',
       cityId:'',
 
-
+      //把持情報
       dropdownWide: [],
       dropdownClose: [],
       dropdown_close_selected:[],
@@ -32,34 +42,6 @@ export default{
 
 
   methods:{
-    // クリックするとAPI取得ができるメソッド
-    //天気情報の取得
-    getWeatherData: function () {
-      let url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + this.cityCode//Idにすると404が出る、要質問
-      // リストIdから情報の取得
-      //APIの読み込み
-      axios
-        .get(url)
-        .then(response => {
-          for(var num=0; num<response['data']['forecasts'].length; num++) {
-            this.dateLabel = response['data']['forecasts'][this.num]["dateLabel"]
-            this.telop = response['data']['forecasts'][this.num]["telop"]
-            // this.iconUrl = response['data']['forecasts'][this.num]["image"]["url"]
-            // this.date = response['data']['forecasts'][this.num]["date"]
-            this.min=response['data']['forecasts'][num]["temperature"]["min"]["celsius"]
-            this.max=response['data']['forecasts'][num]["temperature"]["max"]["celsius"]
-            this.text = response['data']['description']['text']
-            // this.info=response['data']['forecasts'][this.num]
-            console.log(response['data'])
-          }
-        })
-        .catch(err => {
-          this.info = 'fault to get API'
-        })
-    },
-
-
-
     //XMLからリスト情報の取得
     getLocation: function(){
       axios
@@ -93,21 +75,53 @@ export default{
     },
     
     
-    
-    filterCity: function (areaName=this.area) {
+    //エリア情報から都市の絞り込み
+    filterCity: function () {
       // area と同じ text (都市名)のものを selected に入れる
       //表示配列の初期化
       this.dropdownCloseSelected = []
       //引数のエリア名に一致する要素をフィルターして代入
-      this.dropdownCloseSelected = this.dropdownClose.filter(dropdownClose => dropdownClose.area === areaName)
+      this.dropdownCloseSelected = this.dropdownClose.filter(dropdownClose => dropdownClose.area === this.area)
 
-      // console.log('filterCity')
-      // console.log(this.dropdownCloseSelected)
+      console.log('filterCity')
+      console.log(this.dropdownCloseSelected)
     },
 
 
+    //都市コードのセット
+    setCityId: function(){
+      console.log("position check")
+      //.attribute はxml用 　連想配列は次のようにドットを打つだけ
+      let code = this.dropdownCloseSelected.filter(dropdownCloseSelected => dropdownCloseSelected.text === this.city)[0].id
+      this.requestUrl = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + code
+      console.log("requestURL")
+      console.log(this.requestUrl)
+    },
 
-
+    //天気情報の取得
+    getWeatherData: function () {
+      let url = this.requestUrl
+      // リストIdから情報の取得
+      //APIの読み込み
+      axios
+        .get(url)
+        .then(response => {
+          for(let num=0; num<response['data']['forecasts'].length; num++) {
+            this.dateLabel = response['data']['forecasts'][this.num]["dateLabel"]
+            this.telop = response['data']['forecasts'][this.num]["telop"]
+            // this.iconUrl = response['data']['forecasts'][this.num]["image"]["url"]
+            this.date = response['data']['forecasts'][this.num]["date"]
+            this.min=response['data']['forecasts'][this.num]["temperature"]["min"]["celsius"]
+            this.max=response['data']['forecasts'][this.num]["temperature"]["max"]["celsius"]
+            this.text = response['data']['description']['text']
+            // this.info=response['data']['forecasts'][this.num]
+            console.log(response['data'])
+          }
+        })
+        .catch(err => {
+          this.info = 'fault to get API'
+        })
+    },
 
 
     // イベントハンドラ
@@ -119,7 +133,11 @@ export default{
 
     onSetCloseArea: function(){
       //都市選択時のイベント
+      //cityIdの設定
+      this.setCityId()
+      //axiosでの天気情報取得
       this.getWeatherData()
+      //表示用に天気情報を入れる
     }
 
   },
