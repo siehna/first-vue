9c936348fd51ea4254963b895b8acc746a648495
siehna <bq13026@gmail.com>
2019-03-15 16:54:39 +0900

    v-forでの表記できた！ 次はサブコンポーネント化

--- src/components/Weather/script.js
+++ src/components/Weather/script.js
@@ -5,17 +5,9 @@ export default {
   data() {
     return {
       //表示情報
-      weatherInfo: {
-        dateLabel: [],
-        telop: [],
-        date: [],
-        temperature: {
-          max: [],
-          min: []
-        },
-        weatherSummary: '',
-        iconUrl: [],
-      },
+      weatherInfo: [],
+      //表示情報その2
+      weatherSummary: 'no text',
 
       //各種v-モデル用のitem
       prefectures: [],
@@ -37,6 +29,7 @@ export default {
         .get('http://weather.livedoor.com/forecast/rss/primary_area.xml', {responseType: 'document'})
         .then(responseXml => {
           let prefTags = responseXml['data'].getElementsByTagName('pref')
+          //XMLはデータ処理をしないといけないのだ・・・
           //地域リスト作成
           let prefLength = prefTags.length
           let prefectures = []
@@ -63,6 +56,7 @@ export default {
 
             prefectures.push(prefecture)
           }
+
           this.prefectures = prefectures
         })
     },
@@ -70,7 +64,6 @@ export default {
 
     //都市コードのセット
     setCityId: function () {
-      console.log('#### setCityId')
       //.attribute はxml用 　連想配列は次のようにドットを打つだけ
       let code = this.selectedCity.id
       this.requestUrl = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + code
@@ -79,60 +72,39 @@ export default {
 
     //天気情報の取得
     getWeatherData: function () {
-      // リストIdから情報の取得
-
+      // カードの情報をつっこむ
       //　thisが使えないので変数に入れて使う ついでにweather付けとくとコード量減
-      let self = this.weatherInfo
+      let self = this
 
       //APIの読み込み
       axios
         .get(this.requestUrl)
         .then(response => {
-          let forecasts = response.data.forecasts
-
-          for (let i = 0; i < forecasts.length; i++) {
-            // 情報の取得
-            //今日明日明後日
-            if (forecasts[i].dateLabel === null) {
-            } else {
-              self.dateLabel[i] = forecasts[i].dateLabel
+          self.weatherInfo = response.data.forecasts
+          for(let i=0; i<self.weatherInfo.length; i++){
+            let temperature = self.weatherInfo[i].temperature
+            if(temperature.min === null){
+              // temperature.min.push({celsius:'-'})
+              temperature.min = {
+                celsius: '-'
+              }
             }
-            //天気
-            if (forecasts[i].telop === null) {
-              self.telop[i] = '-'
-            } else {
-              self.telop[i] = forecasts[i].telop
-            }
-            //アイコン
-            if (forecasts[i].image.url === null) {
-            } else {
-              self.iconUrl[i] = forecasts[i].image.url
-            }
-            // 日付
-            if (forecasts[i].date === null) {
-              self.date[i] = '-'
-            } else {
-              self.date[i] = forecasts[i].date
-            }
-            //最高・最低気温の取得
-            if (forecasts[i].temperature.min === null) {
-              self.temperature.min[i] = '-'
-            } else {
-              self.temperature.min[i] = forecasts[i].temperature.min.celsius
-            }
-            if (forecasts[i].temperature.max === null) {
-              self.temperature.max[i] = '-'
-            } else {
-              self.temperature.max[i] = forecasts[i].temperature.max.celsius
-            }
-            //お天気アイコンの取得
-            if (forecasts[i].image.url === null) {
-            } else {
-              self.iconUrl[i] = forecasts[i].image.url
+            if(temperature.max === null){
+              // temperature.max.celsius = '-'
+              temperature.max = {
+                celsius: '-'
+              }
             }
           }
-          //for 文ここまで
 
+          console.log('weatherInfo')
+          console.log(self.weatherInfo)
+          //
+          // console.log('url')
+          // console.log(self.weatherInfo[0].image.url)
+          //
+          // console.log('max')
+          // console.log(self.weatherInfo[0].temperature.max.celsius)
           self.weatherSummary = response.data.description.text
         })
         .catch(err => {
