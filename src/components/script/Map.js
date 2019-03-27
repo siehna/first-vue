e1ecd5a5bb472e44fb2a84daef664b1dc638c01a
siehna <bq13026@gmail.com>
2019-03-27 15:18:10 +0900

    map出た

--- src/components/script/Map.js
+++ src/components/script/Map.js
@@ -1,8 +1,19 @@
 
-
-
 export default {
+  methods:{
+    getMap:function(){
+      let MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516)
+      let Options = {
+        zoom: 15,      //地図の縮尺値
+        center: MyLatLng,    //地図の中心座標
+        mapTypeId: 'roadmap'   //地図の種類
+      };
+      let map = new google.maps.Map(document.getElementById('map'), Options)
+    }
+  },
+
   mounted: function(){
     eventHub.$emit('change-title','Map')
+    this.getMap()
   }
 }
--- src/components/vue/Map.vue
+++ src/components/vue/Map.vue
@@ -1,3 +1,4 @@
 <template src="../template/Map.html"></template>
 <script src="../script/Map.js"></script>
 <style scoped src="../style/Map.css"></style>
+
