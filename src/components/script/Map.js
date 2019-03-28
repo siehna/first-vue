dad24aa441bd25d0845f26ac791d12352be63801
siehna <bq13026@gmail.com>
2019-03-28 14:06:32 +0900

    プロジェクト名を変更した

--- src/components/script/Map.js
+++ src/components/script/Map.js
@@ -4,11 +4,13 @@ export default {
     getMap:function(){
       let MyLatLng = new google.maps.LatLng(35.6811673, 139.7670516)
       let Options = {
-        zoom: 15,      //地図の縮尺値
-        center: MyLatLng,    //地図の中心座標
-        mapTypeId: 'roadmap'   //地図の種類
-      };
+        zoom: 15,
+        // scrollwheel:false,
+        center: MyLatLng,
+        mapTypeId: 'roadmap'
+      }
       let map = new google.maps.Map(document.getElementById('map'), Options)
+      console.log(map.getCenter())
     }
   },
 
